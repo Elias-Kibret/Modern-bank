1c49e1a3500e22e61b63447996d1c5278fe8e172
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-15 10:21:26 -0700

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./style";
+import { Navbar,Hero,Billing,Business,Stats} from "./components/index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden h-[100vh]">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+     <Navbar/>
+      </div>
     </div>
-  );
-}
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+    <Hero/>
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Billing/>
+        <Business/>
+        <Stats/>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/components/Stats.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <div className='text-yellow-900'>Hello</div>
+  )
+}
+
+export default Stats
