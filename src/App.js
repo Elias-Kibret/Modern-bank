53c71cecea1a305d633f39a1b7bac7a5e2bb1357
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-15 14:23:03 -0700

    stats

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import styles from "./style";
 import { Navbar,Hero,Billing,Business,Stats} from "./components/index";
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden h-[100vh]">
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
      <Navbar/>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import { discount,robot } from '../assets'
 import GetStarted from './GetStarted'
 const Hero = () =>  (
     <section id="home"
-             className={`flex md:flex-col flex-row ${styles.paddingY}`}
+             className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
        <div className={`flex-1 ${styles.flexStart}
                      flex-col xl:px-0 sm:px-16 px-6`}>
@@ -36,7 +36,20 @@ const Hero = () =>  (
                                                </div>
                                                  </div>
                                                  <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px]'>Payment Method</h1>
-                                                 <p></p>
+                                                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the creadit
+                                                  credit cards most likely to fit your needs 
+                                                  we examine annual percentage rates, annual fees
+                                                 </p>
+                        </div>
+                        <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+                          <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
+                          <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
+                          <div className='absolute z-[1] w-[80%] rounded-full
+                           bottom-40 h-[80%] top-0 white__gradient'></div>
+                          <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 top-0 blue__gradient'></div>
+                        </div>
+                        <div className={`ss:hidden ${styles.flexCenter}`}>
+                           <GetStarted/>
                         </div>
                     
 
