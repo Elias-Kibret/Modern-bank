4cf7e464fd57e4e01324a026ce8306b5365a22dc
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-15 13:43:54 -0700

    hero

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styles from '../style'
+import { arrowUp } from '../assets'
+const GetStarted = () => (
+    <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+<div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
+    
+   <div className={`${styles.flexStart} flex-row items-center`}>
+    <p className='font-poppins font-medium text-[18px]'>
+        <span className='text-gradient'>Get</span>
+        
+    </p>
+    <img src={arrowUp} alt="arrow up"
+         className='w-[23px] h-[23px] object-contain  ml-[3px]'
+    />
+   </div>
+   <p className='font-poppins font-medium text-[18px]'>
+        <span className='text-gradient'>Started</span>
+        
+    </p>
+</div>
+    </div>
   )
-}
+
 
 export default GetStarted
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import styles from '../style'
+import { discount,robot } from '../assets'
+import GetStarted from './GetStarted'
+const Hero = () =>  (
+    <section id="home"
+             className={`flex md:flex-col flex-row ${styles.paddingY}`}
+    >
+       <div className={`flex-1 ${styles.flexStart}
+                     flex-col xl:px-0 sm:px-16 px-6`}>
+                        <div className={`flex flex-row first-letter:
+                                          items-center py-[6px] px-4
+                                          bg-discount-gradient rounded-[10px] mb-2
+                        `}>
+                          <img 
+                                src={discount}
+                                 alt="discount"
+                                 className="w-[32px] h-[32px]"
+                                 />
+                                 <p>
+                                 <span className='text-white'>20% </span>
+                                 <span className='text-gray-400'>DISCOUNT </span>
+                                 <span className='text-white'>1 MONTH</span>
+                                 <span className='text-gray-400'> ACCOUNT </span>
+                                 </p>
+                                 </div>
+                                                 <div className='flex flex-row 
+                                                            justify-between items-center w-full'>
+                               <h1 className='flex-1 
+                                              font-poppins font-semibold 
+                                              ss:text-[72px] text-[52px]
+                                               text-white ss:leading-[100px]'>The Next <br className='sm:block hidden'/>{" "} <span className='text-gradient'>Generation</span><br/> {" "}  </h1>
 
-const Hero = () => {
-  return (
-    <div className='text-yellow-900'>Hello</div>
+                                               <div>
+                                                <GetStarted/>
+                                               </div>
+                                                 </div>
+                                                 <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px]'>Payment Method</h1>
+                                                 <p></p>
+                        </div>
+                    
+
+    </section>
   )
-}
+
 
 export default Hero
