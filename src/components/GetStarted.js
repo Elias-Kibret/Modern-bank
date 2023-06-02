1a693a2f5b704509b66b4bb9303ae96a98cd11cb
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-06-02 10:31:50 +0300

    edited

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import styles from '../style'
-import { arrowUp } from '../assets'
+import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 const GetStarted = () => (
-    <div
-    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-<div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+	<div
+		className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+	>
+		<div
+			className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+		>
+			<div className={`${styles.flexStart} flex-row items-center`}>
+				<p className="font-poppins font-medium text-[18px]">
+					<span className="text-gradient">JOIN</span>
+				</p>
+			</div>
+			<p className="font-poppins font-medium text-[18px]">
+				<span className="text-gradient">US</span>
+			</p>
+		</div>
+	</div>
+);
 
-    
-   <div className={`${styles.flexStart} flex-row items-center`}>
-    <p className='font-poppins font-medium text-[18px]'>
-        <span className='text-gradient'>Get</span>
-        
-    </p>
-    <img src={arrowUp} alt="arrow up"
-         className='w-[23px] h-[23px] object-contain  ml-[3px]'
-    />
-   </div>
-   <p className='font-poppins font-medium text-[18px]'>
-        <span className='text-gradient'>Started</span>
-        
-    </p>
-</div>
-    </div>
-  )
-
-
-export default GetStarted
+export default GetStarted;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,76 +1,89 @@
-import React from 'react'
-import styles from '../style'
-import { discount,robot } from '../assets'
-import GetStarted from './GetStarted'
-import { motion } from 'framer-motion'
-const Hero = () =>  (
-    <section id="home"
-             className={`flex md:flex-row flex-col ${styles.paddingY}`}
-    >
-       <motion.div 
-      
-       className={`flex-1 ${styles.flexStart}
-                     flex-col xl:px-0 sm:px-16 px-6`}>
-                        <div className={`flex flex-row first-letter:
+import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+import { motion } from "framer-motion";
+const Hero = () => (
+	<section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+		<motion.div
+			className={`flex-1 ${styles.flexStart}
+                     flex-col xl:px-0 sm:px-16 px-6`}
+		>
+			<div
+				className={`flex flex-row first-letter:
                                           items-center py-[6px] px-4
                                           bg-discount-gradient rounded-[10px] mb-2
-                        `}>
-                          <img 
-                                src={discount}
-                                 alt="discount"
-                                 className="w-[32px] h-[32px]"
-                                 />
-                                 <p>
-                                 <span className='text-white'>20% </span>
-                                 <span className='text-gray-400'>DISCOUNT </span>
-                                 <span className='text-white'>1 MONTH</span>
-                                 <span className='text-gray-400'> ACCOUNT </span>
-                                 </p>
-                                 </div>
-                                                 <div className='flex flex-row 
-                                                            justify-between items-center w-full'>
-                               <motion.h1 
-                               initial={{x:-300,opacity:0.5}}
-                               whileInView={{x:0,opacity:1}}
-                               transition={{duration:.25}}
-                               
-                               className='flex-1 
+                        `}
+			>
+				<img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+				<p>
+					<span className="text-white">WEB </span>
+					<span className="text-gray-400">APP </span>
+					<span className="text-white">UI/UX</span>
+					<span className="text-gray-400"> </span>
+				</p>
+			</div>
+			<div
+				className="flex flex-row 
+                                                            justify-between items-center w-full"
+			>
+				<motion.h1
+					initial={{ x: -300, opacity: 0.5 }}
+					whileInView={{ x: 0, opacity: 1 }}
+					transition={{ duration: 0.25 }}
+					className="flex-1 
                                               font-poppins font-semibold 
                                               ss:text-[72px] text-[52px]
-                                               text-white ss:leading-[100px]'>The Next <br className='sm:block hidden'/>{" "} <span className='text-gradient'>Generation</span><br/> {" "}  </motion.h1>
+                                               
+                                               text-white ss:leading-[100px]"
+				>
+					The future of <br className="sm:block hidden" />{" "}
+					<span className="text-gradient">Technology</span>
+					<br />{" "}
+				</motion.h1>
 
-                                               <div>
-                                                <GetStarted/>
-                                               </div>
-                                                 </div>
-                                                 <motion.h1 
-                                                 initial={{scale:0.6,opacity:.4}}
-                                                 whileInView={{scale:1,opacity:1}}
-                                                 transition={{duration:0.3}}
-                                                 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px]'>Payment Method</motion.h1>
-                                                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the creadit
-                                                  credit cards most likely to fit your needs 
-                                                  we examine annual percentage rates, annual fees
-                                                 </p>
-                        </motion.div>
-                        <motion.div 
-                         initial={{x:300,opacity:0.5}}
-                         whileInView={{x:0,opacity:1}}
-                         translate={{duration:.5, delay:0.2}}
-                        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-                          <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
-                          <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
-                          <div className='absolute z-[1] w-[80%] rounded-full
-                           bottom-40 h-[80%] top-0 white__gradient'></div>
-                          <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 top-0 blue__gradient'></div>
-                        </motion.div>
-                        <div className={`ss:hidden ${styles.flexCenter}`}>
-                           <GetStarted/>
-                        </div>
-                    
+				<div>
+					<GetStarted />
+				</div>
+			</div>
+			<motion.h1
+				initial={{ scale: 0.6, opacity: 0.4 }}
+				whileInView={{ scale: 1, opacity: 1 }}
+				transition={{ duration: 0.3 }}
+				className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px]"
+			>
+				is here.
+			</motion.h1>
+			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+				We are a team of passionate and talented engineers who are committed to
+				using our skills and knowledge to build innovative products that make a
+				difference in the world. We believe that technology has the power to
+				solve some of the world's biggest problems, and we are dedicated to
+				using our skills to make that happen.
+			</p>
+		</motion.div>
+		<motion.div
+			initial={{ x: 300, opacity: 0.5 }}
+			whileInView={{ x: 0, opacity: 1 }}
+			translate={{ duration: 0.5, delay: 0.2 }}
+			className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+		>
+			<img
+				src={robot}
+				alt="billing"
+				className="w-[100%] h-[100%] relative z-[5]"
+			/>
+			<div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
+			<div
+				className="absolute z-[1] w-[80%] rounded-full
+                           bottom-40 h-[80%] top-0 white__gradient"
+			></div>
+			<div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 top-0 blue__gradient"></div>
+		</motion.div>
+		<div className={`ss:hidden ${styles.flexCenter}`}>
+			<GetStarted />
+		</div>
+	</section>
+);
 
-    </section>
-  )
-
-
-export default Hero
+export default Hero;
