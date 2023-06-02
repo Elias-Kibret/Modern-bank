21b652255370d65a21e9fe5cc4f85958699ec508
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-06-02 13:34:07 +0300

    company

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import styles from "./style";
-import { Navbar,Hero,Billing,Business,Stats, CardDeal, Testimonials, Clients,CTA,Footer} from "./components/index";
+import {
+	Navbar,
+	Hero,
+	Billing,
+	Business,
+	Stats,
+	CardDeal,
+	Testimonials,
+	Clients,
+	CTA,
+	Footer,
+} from "./components/index";
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-     <Navbar/>
-      </div>
-    </div>
+	<div className="bg-primary w-full overflow-hidden">
+		<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Navbar />
+			</div>
+		</div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-    <Hero/>
-      </div>
-    </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats/>
-        <Business/>
-        <Billing/>
-        <CardDeal/>
-        <Testimonials/>
-        <Clients/>
-        <CTA/>
-        <Footer/>
-       
-        
-      </div>
-    </div>
-  </div>
+		<div className={`bg-primary ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Hero />
+			</div>
+		</div>
+
+		<div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Stats />
+				<Business />
+				<Billing />
+				<Testimonials />
+				<CardDeal />
+				<Clients />
+				<CTA />
+				<Footer />
+			</div>
+		</div>
+	</div>
 );
 
 export default App;
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -13,12 +13,6 @@ const Testimonials = () => (
 			<h2 className={styles.heading2}>
 				Founders <br className="sm:block hidden" />
 			</h2>
-			<div className="w-full md:mt-0 mt-6">
-				<p className={`${styles.paragraph} text-left max-w-[470px]`}>
-					Everything you need to accept card payments and grow your business
-					anywhere on the planet.
-				</p>
-			</div>
 		</div>
 		<div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
 			{feedback.map((card, index) => (
